a9509d104e7157e14b6908ab60dec60d1eb1f653
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-31 11:31:30 -0300

    Added update code command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+aws lambda update-function-code --function-name ${NODIS_FUNCTION_NAME} --s3-bucket ${NODIS_ARTIFACT_BUCKET} --s3-key ${NODIS_ARTIFACT_PATH}/${NODIS_ARTIFACT_FULLNAME}
+
+
 
